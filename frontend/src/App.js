6806058e68a5ffb46c6bca4014a2217456a7bea2
akurathipravallika545-cloud <akurathipravallika545@gmail.com>
2025-12-07 22:57:36 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+project-root/
+│
+├── frontend/
+│   ├── src/
+│   │   ├── components/
+│   │   │   ├── PDFViewer.js
+│   │   │   ├── DraggableField.js
+│   │   ├── App.js
+│   │   ├── index.js
+│   ├── package.json
+│   ├── vite.config.js (if using Vite)
+│
+├── backend/
+│   ├── server.js
+│   ├── package.json
+│   ├── db.js
+│
+└── README.md
