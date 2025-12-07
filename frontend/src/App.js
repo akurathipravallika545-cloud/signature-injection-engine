bb75544ac5d410988767db13a07f5cc860348e63
akurathipravallika545-cloud <akurathipravallika545@gmail.com>
2025-12-07 23:10:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,1 @@
-project-root/
-│
-├── frontend/
-│   ├── src/
-│   │   ├── components/
-│   │   │   ├── PDFViewer.js
-│   │   │   ├── DraggableField.js
-│   │   ├── App.js
-│   │   ├── index.js
-│   ├── package.json
-│   ├── vite.config.js (if using Vite)
-│
-├── backend/
-│   ├── server.js
-│   ├── package.json
-│   ├── db.js
-│
-└── README.md
+
